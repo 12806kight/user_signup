1e30ff29e635bdc6591c9ab5aaaf329cd76b7222
Brewer Kight <bkight@atlassian.com>
2019-06-26 01:20:01 -0500

    We have updated username and password

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-form = """
+user_form = """
 <!DOCTYPE html>
 
 <html>
@@ -14,22 +14,23 @@ form = """
     </head>
     <body>
       <form method="POST">
-        <label for="Username">Username:</label>
-  		<input type="text" name="Username" id="Username" required>
-        <br>
-        <label for="Password">Password:</label>
-  		<input type="password" name="Password" id="Password" required>
-        <br>
-        <label for="Verify">Verify:</label>
-  		<input type="password" name="Verify" id="Verify" required>
-        <br>
-        <label for="email">Email(optional):</label>
-  		<input type="text" name="email" id="email" >
-        <br>
+        <label>Username:
+          <input name="username" type="text" value='{username}' required>
+        </label>
+        <p>{username_error}</p>
+        <br><br>
+        <label>Password:
+          <input name="password" type="text" value='{password}' required>
+        </label>
+        <p>{password_error}</p>
+        <br><br>
+
+        <br><br>
 
 
 
-        
+
+
         <input type="submit" value="Submit">
            
       </form>
@@ -39,6 +40,27 @@ form = """
 
 @app.route("/")
 def index():
-    return form.format("")
+    return user_form.format(username='', username_error='', password='', password_error='')
 
+@app.route("/", methods=['POST'])
+def validate():
+
+    username = request.form['username']
+    password = request.form['password']
+
+    username_error = ''
+    password_error = ''
+
+    if len(username) < 3 or len(username) > 30:
+      username_error = 'Username should be inbetween 2 to 30 letters'
+      username = ''
+
+    if len(password) < 3 or len(password) > 30:
+      password_error = 'Password Error'
+      password = ''
+
+    if not username_error and not password_error:
+      return "Welcome, {{username}}"
+    else: 
+      return user_form.format(username_error=username_error, password_error=password_error, username=username, password=password)
 app.run()
